b9ec823d8278ac34d5c3718555c508d0ed716471
Naoki Hatayama <naoki42070@gmail.com>
2026-08-28 02:47:36 +0900

    feat:  タイトルシーンと遷移するシステム

---
+++ Assets/Scripts/GameSetting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class GameSetting
+{
+    public static bool IsSoloMode = false;
+    public static bool IsResearchMode = false;
+
+    public static string Player1Name = "1P";
+    public static string Player2Name = "2P";
+}
