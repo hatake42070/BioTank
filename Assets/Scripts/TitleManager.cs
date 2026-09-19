cbacd6de4a28b6aafd5e709fa76c5cabfc6c7bef
畑山直輝 <naoki42070@gmail.com>
2026-09-20 00:18:14 +0900

    chore: オプションパネルの作成

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -3,10 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    [Header("UI Panels")] 
+    [SerializeField] private CanvasGroup mainMenuPanel;
+    [SerializeField] private CanvasGroup optionsPanel;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // オプションパネルを閉じる
+        CloseOption();
     }
 
     // Update is called once per frame
@@ -25,5 +30,30 @@ public class TitleManager : MonoBehaviour
     {
         GameSetting.IsSoloMode = true;
         // SceneManager.LoadScene("MainScene");
+    }
+
+    public void OpenOption()
+    {
+        mainMenuPanel.alpha = 0;
+        mainMenuPanel.interactable = false;
+        mainMenuPanel.blocksRaycasts = false;
+        
+        optionsPanel.alpha = 1;
+        optionsPanel.interactable = true;
+        optionsPanel.blocksRaycasts = true;
+    }
+
+    // オプションパネルの中の閉じるボタンを押された時に呼ばれる
+    public void CloseOption()
+    {
+        // オプション画面を透明・操作不可にする
+        optionsPanel.alpha = 0;
+        optionsPanel.interactable = false; // パネルの中にあるUIを操作不能にする
+        optionsPanel.blocksRaycasts = false; // パネル全体の当たり判定をなくす
+        
+        // メインメニューを表示・操作可能にする
+        mainMenuPanel.alpha = 1;
+        mainMenuPanel.interactable = true;
+        optionsPanel.blocksRaycasts = true;
     }
 }
